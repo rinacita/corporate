ef95228b4fb07511f4ab9b3280ba0c04b3877321
kuon609 <kuon@prog-8.com>
2019-02-02 18:42:08 +0900

    fix width

--- corp/components/MainVisual.vue
+++ corp/components/MainVisual.vue
@@ -5,28 +5,28 @@
     p.small 感動とワクワクで世界を彩る
   .wrapper
     .top.flex
-      .square(style='width: 13.28125vw')
+      .square(style='width: 2.55em')
         img(src='~/assets/img/draw.jpg')
-      .square.black(style='width: 8.541667vw')
-      .square(style='width: 10.8333333vw')
+      .square.black(style='width: 1.7em')
+      .square(style='width: 2.08em')
         img(src='~/assets/img/code.jpg')
-      .square(style='width: 15.625vw')
+      .square(style='width: 3em')
         img(src='~/assets/img/camera.jpg')
     .bottom.flex
       .left.flex
-        .square.yellow(style='width: 10.8333333vw')
-        .square(style='width: 22.0833333vw')
+        .square.yellow(style='width: 2.08em')
+        .square(style='width: 4.24em')
           img(src='~/assets/img/live.jpg')
       .right.flex
         .right-top.flex
-          .square(style='width: 16.4583333vw')
+          .square(style='width: 3.16em')
             img(src='~/assets/img/sticker.jpg')
-          .square(style='width: 16.4583333vw')
+          .square(style='width: 3.16em')
             img(src='~/assets/img/load.jpg')
         .right-bottom.flex
-          .square(style='width: 26.0416667vw')
+          .square(style='width: 5em')
             img(src='~/assets/img/studio.jpg')
-          .square(style='width: 18.4895833vw')
+          .square(style='width: 3.55em')
             img(src='~/assets/img/frontman.jpg')
 </template>
 <style lang="scss" scoped>
@@ -37,6 +37,8 @@
   height: 100vh;
   text-align: center;
   position: relative;
+  font-size: 100px;
+  margin-bottom: 200px;
 }
 .copy {
   position: absolute;
@@ -45,6 +47,9 @@
   transform: translate3d(-50%, -50%, 0);
   z-index: 100;
   text-align: left;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 }
 .big {
   font-family: futura-pt-bold;
@@ -72,7 +77,7 @@
   align-items: flex-end;
   transform: rotate(45deg);
   position: relative;
-  left: -3%;
+  left: -4%;
 }
 .square {
   width: 24.6785%;
@@ -83,11 +88,11 @@
 }
 .black {
   background: $black;
-  height: 8.541667vw;
+  height: 1.7em;
 }
 .yellow {
   background: $yellow;
-  height: 10.8333333vw;
+  height: 2.08em;
 }
 .flex {
   display: flex;
