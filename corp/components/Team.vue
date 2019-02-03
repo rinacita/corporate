b4afb3b1ff098771cc71415d69c98b35359377fc
kuon609 <kuon@prog-8.com>
2019-02-03 21:29:02 +0900

    add logic

--- corp/components/Team.vue
+++ corp/components/Team.vue
@@ -2,15 +2,40 @@
 container
   section-title.mb64(align="center") TEAM
   .flex
-    member(v-for="(member, index) in activeMembers" :key="member.id")
-      img(slot='image' :src="require('~/assets/img/' + member.image + '.jpg')")
-      h3(slot='name') {{member.name}}
-      tag(v-for="position in member.position").margin-right(slot='position') {{position}}
+    .member(v-for="(member, index) in activeMembers" :key="member.id" @click="open(index)")
+      .image
+        img(:src="require('~/assets/img/' + member.image + '.jpg')")
+      div
+        .name {{member.name}}
+        tag(v-for="position in member.position").margin-right {{position}}
+  no-ssr
+    modal(
+      name='detail'
+      pre-mount=true
+    )
+      .modal-inner
+        .modal-image
+          img(:src="require('~/assets/img/' + members[modalIndex].image + '.jpg')")
+        .modal-info
+          .modal-top
+            .modal-name {{members[modalIndex].name}}
+            tag(v-for="position in members[modalIndex].position").margin-right {{position}}
+            .link
+              a.sns(v-if="members[modalIndex].twitter" :href="'https://twitter.com/' + members[modalIndex].twitter" target="blank")
+                i.fab.fa-twitter
+              a.sns(v-if="members[modalIndex].facebook" :href="'https://www.facebook.com/' + members[modalIndex].facebook" target="blank")
+                i.fab.fa-instagram
+          .modal-text(v-if="members[modalIndex].history") {{members[modalIndex].history}}
+        .direction
+          div(@click="previous")
+            i.fab.fa-twitter
+          div(@click="next")
+            i.fab.fa-instagram
+
 </template>
 <script>
 import Container from '~/components/atoms/Container'
 import SectionTitle from '~/components/atoms/SectionTitle'
-import Member from '~/components/Member'
 import Tag from '~/components/atoms/Tag'
 import teamData from '~/assets/data/team.json'
 
@@ -18,12 +43,12 @@ export default {
   components: {
     Container,
     SectionTitle,
-    Member,
     Tag
   },
   data: () => {
     return {
-      members: teamData
+      members: teamData,
+      modalIndex: 0
     }
   },
   computed: {
@@ -31,6 +56,26 @@ export default {
       return this.members.filter(member => {
         return member.isActive
       })
+    }
+  },
+  methods: {
+    open(index) {
+      this.modalIndex = index
+      this.$modal.push('detail')
+    },
+    close() {
+      this.$modal.pop()
+    },
+    previous() {
+      if (this.modalIndex > 0) {
+        this.modalIndex--
+      }
+    },
+    next() {
+      const length = this.activeMembers.length - 1
+      if (this.modalIndex < length) {
+        this.modalIndex++
+      }
     }
   }
 }
@@ -65,5 +110,20 @@ export default {
   &:last-child {
     margin-right: 0;
   }
+}
+.member {
+  width: 21.666%;
+  margin-bottom: 48px;
+  @include scaleHover();
+}
+.image {
+  width: 100%;
+  margin-bottom: 12px;
+}
+.name {
+  font-size: 1.8rem;
+  font-weight: 900;
+  letter-spacing: 0.1em;
+  margin-bottom: 6px;
 }
 </style>
